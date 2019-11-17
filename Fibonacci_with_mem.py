41a9983649cc12839ef39d632e1b763f6551c003
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-11-18 00:11:38 +0300

    Create Fibonacci_with_mem.py

---
+++ Fibonacci_with_mem.py
@@ -0,0 +1,12 @@
+_fib_cache = {1: 1, 2: 1}  # ключ - номер числа, значения - число Фибоначчи 
+
+n = input("Номер элемента ряда Фибоначчи: ")
+n = int(n)
+def mem_fib(n):
+    result = _fib_cache.get(n)
+    if result is None:
+        result = mem_fib(n-2) + mem_fib(n-1)
+        _fib_cache[n] = result
+    return result
+
+print('Ответ: ', mem_fib(n))
