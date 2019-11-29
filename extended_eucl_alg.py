4eedf6d507a036f528513b8a3d1ad737336081f7
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-11-29 22:50:04 +0300

    Create extended_eucl_alg.py

---
+++ extended_eucl_alg.py
@@ -0,0 +1,10 @@
+def bezout(a, b):
+    x, xx, y, yy = 1, 0, 0, 1
+    while b:
+        q = a // b
+        a, b = b, a % b
+        x, xx = xx, x - xx*q
+        y, yy = yy, y - yy*q
+    return (x, y, a)
+
+bezout(666,13)
