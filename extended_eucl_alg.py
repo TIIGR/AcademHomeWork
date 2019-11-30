b4ae81377d0f094812d72b58b8174229b6cecb3a
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-11-30 15:32:01 +0300

    Update extended_eucl_alg.py

--- extended_eucl_alg.py
+++ extended_eucl_alg.py
@@ -1,7 +1,8 @@
 def bezout(a, b):
     if not b:
-        return (1, 0, a)
+        return 1, 0, a
     y, x, g = bezout(b, a % b)
-    return (x, y - (a // b) * x, g)
+    return x, y - (a // b) * x, g
 
-bezout(666,13)
+
+bezout(666, 13)
