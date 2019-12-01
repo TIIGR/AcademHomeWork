93d6f9204a3e592eb8c603320b42e52e32b5fa2a
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-12-01 19:22:59 +0300

    Update prime_test.py

--- 4_izi_balla/prime_test.py
+++ 4_izi_balla/prime_test.py
@@ -1,12 +1,9 @@
-def prime(n):
-    if n == 1:
-        return ()
-    if n == 2:
-        return True
-    for i in range(2, round(n ** (1 / 2)) + 1):
-        if n % i == 0:
-            return False
-    return True
+def Prime(n):
+    d = 2
+    while n % d != 0:
+        d += 1
+    return d == n
 
 
-print(prime(100500))
+a = int(input("Введите число: "))
+print(Prime(a))
