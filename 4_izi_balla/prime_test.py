19b12be940c51ec9e969aa7e97525673e24857f1
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-12-01 20:05:37 +0300

    Update prime_test.py

--- 4_izi_balla/prime_test.py
+++ 4_izi_balla/prime_test.py
@@ -1,12 +1,15 @@
 def Prime(n):
-    if n == 1:
-        return "True"
+    if n <= 0:
+        return "it's not a natural number!"
     else:
-        d = 2
-        while n % d != 0:
-            d += 1
-        return d == n
-    
+        if n == 1:
+            return "True"
+        else:
+            d = 2
+            while n % d != 0:
+                d += 1
+            return d == n
 
-a = int(input("Введите натуральное число: "))
+
+a = int(input("Введите число: "))
 print(Prime(a))
