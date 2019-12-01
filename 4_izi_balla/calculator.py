d84abc4c7d8f609864e44b4a17358052210b75d2
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-12-01 23:04:35 +0300

    Update calculator.py

--- 4_izi_balla/calculator.py
+++ 4_izi_balla/calculator.py
@@ -1,15 +1,19 @@
-def carry(value, original):
-    if isinstance(value, str):
-        n = int(value)
+def calc(num, base, target):
+    if b <= 0 or c <= 0 or b > 36 or c > 36:
+        return "Введенные вами сс, противоречат возможностям программы"
     else:
-        n = int(value)
-    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if n < original:
-        return alphabet[n]
-    else:
-        return carry(n // original, original) + alphabet[n % original]
+        if isinstance(num, str):
+            n = int(num, base)
+        else:
+            n = int(num)
+        alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        if n < target:
+            return alphabet[n]
+        else:
+            return calc(n // target, base, target) + alphabet[n % target]
 
 
-a = int(input("Введите число в 10 сс: "))
-c = int(input("Введите основание конечной системы счисления: "))
-print(carry(a, c))
+a = input("Введите число: ")
+b = int(input("Введите основание сс введенного числа (не превыш. 36): "))
+c = int(input("Введите конечное основание сс (не превыш. 36): "))
+print(calc(a, b, c))
