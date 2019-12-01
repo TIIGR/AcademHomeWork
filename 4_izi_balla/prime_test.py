46e81f2769a5963fe4afc1ed7e3befd9d6252ee7
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-12-01 19:55:13 +0300

    Update prime_test.py

--- 4_izi_balla/prime_test.py
+++ 4_izi_balla/prime_test.py
@@ -1,9 +1,12 @@
 def Prime(n):
-    d = 2
-    while n % d != 0:
-        d += 1
-    return d == n
-
+    if n == 1:
+        return "True"
+    else:
+        d = 2
+        while n % d != 0:
+            d += 1
+        return d == n
+    
 
 a = int(input("Введите число: "))
 print(Prime(a))
