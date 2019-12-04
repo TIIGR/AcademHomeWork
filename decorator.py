5ca13b541be40c2284c50ab03ea0a4a17399a5cb
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-12-04 23:02:32 +0300

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,4 +1,4 @@
-def how_much(o):
+def repetition(o):
     def function(f):
         def argument(x):
             for i in range(o):
@@ -9,11 +9,11 @@ def how_much(o):
 
 
 a = int(input("Ваше число: "))
-y = int(input("Сколько раз прибавлять 3 к числу? "))
+n = int(input("Сколько раз прибавлять 3 к числу? "))
 
 
 # Функция, которая прибавляет к введенному числу 3 в группе целых чисел с вычетом по модулю 5 (Z/5)+.
-@how_much(y)  # Выполняем эту функцию y раз.
+@repetition(n)  # Выполняем эту функцию y раз.
 def sum_in_cycle_sum_group(g):
     g = (g + 3) % 5
     return g
