5506e1428b8f7e9093c3816582280fdfbc4faad2
TIIGR <55460474+TIIGR@users.noreply.github.com>
2019-12-06 17:07:09 +0300

    Create mypy_type_annotation.py

---
+++ mypy_type_annotation.py
@@ -0,0 +1,16 @@
+from typing import TypeVar, List, Union
+
+T = TypeVar('T')  # Можем указывать любой тип данных
+A = TypeVar('A', str, bytes)  # Можем указывать данные типов str и bytes
+
+
+def repeat(x: T, n: int) -> List[T]:  # Функция при вводе x, n выводит массив (список) n элементов состоящих только из x
+    return [x] * n
+
+
+def Longest(x: A, y: A) -> A:  # Функция определяет и возвращает результат такой строки, которая больше другой.
+    return x if len(x) >= len(y) else y
+
+
+def parse_int(value: Union[str, int]) -> int:  # Функция принимает на вход строки и целые числа, но возвр. только числа
+    return int(value)
