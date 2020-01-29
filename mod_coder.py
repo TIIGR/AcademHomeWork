e5b5f44b8b21122bd1572098141d535200c5084c
TIIGR <55460474+TIIGR@users.noreply.github.com>
2020-01-29 23:06:53 +0300

    Update and rename lite_rus_coder.py to mod_coder.py

---
+++ mod_coder.py
@@ -0,0 +1,21 @@
+RUS = '0123456789АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщъыьЭэЮюЯя .,!?"()@/#:-_–'
+
+
+def de_en_code(word):
+    code = ''
+    t = len(word)
+    for Ord in range(0, t):
+        if word[Ord] not in RUS:
+            exit('Присутствует недопустимый символ!')
+        if Q == 0:
+            ind = (RUS.index(word[Ord]) + ((-1) ** Ord) * (Ord + 1)) % 88
+            code = code + RUS[ind]
+        if Q == 1:
+            ind = (RUS.index(word[Ord]) - ((-1) ** Ord) * (Ord + 1)) % 88
+            code = code + RUS[ind]
+    return code
+
+
+Word = str(input('Введите текст: '))
+Q = int(input('Зашифровать (кн. 0) | Расшифровать (кн. 1): '))
+print(de_en_code(Word))
