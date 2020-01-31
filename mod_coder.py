832cb686c42ca7071ff5887bbf3d64567c981ad8
TIIGR <55460474+TIIGR@users.noreply.github.com>
2020-01-31 07:51:31 +0300

    Update mod_coder.py

--- mod_coder.py
+++ mod_coder.py
@@ -1,20 +1,24 @@
-RUS = '0123456789АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщъыьЭэЮюЯя .,!?"()@#:_–'
+RUS = '0123456789АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщъыьЭэЮюЯя .,!?"()@\#:-_–'
 
 
-def de_en_code(word):
+def de_en_code(text):
+    error = 'Присутствуют недопустимые символы: '
     code = ''
-    for Ord in range(0, len(word)):
-        if word[Ord] not in RUS:
-            exit('Присутствует недопустимый символ!')
+    for Ord in range(0, len(text)):
+        if text[Ord] not in RUS:
+            for bug in range(Ord, len(text)):
+                if text[bug] not in RUS:
+                    error = error + text[bug]
+            exit(error)
         if Q == 0:
-            ind = (RUS.index(word[Ord]) + ((-1) ** Ord) * (Ord + 1)) % len(RUS)
+            ind = (RUS.index(text[Ord]) + ((-1) ** Ord) * (Ord + 1)) % len(RUS)
             code = code + RUS[ind]
         if Q == 1:
-            ind = (RUS.index(word[Ord]) - ((-1) ** Ord) * (Ord + 1)) % len(RUS)
+            ind = (RUS.index(text[Ord]) - ((-1) ** Ord) * (Ord + 1)) % len(RUS)
             code = code + RUS[ind]
     return code
 
 
-Word = str(input('Введите текст: '))
+Text = str(input('Введите текст: '))
 Q = int(input('Зашифровать (кн. 0) | Расшифровать (кн. 1): '))
-print(de_en_code(Word))
+print(de_en_code(Text))
