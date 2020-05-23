6633066e6ee5b2c71363bb2d606ac7bc24dab677
TIIGR <55460474+TIIGR@users.noreply.github.com>
2020-05-23 19:21:55 +0300

    update

--- web_app/web.py
+++ web_app/web.py
@@ -3,21 +3,26 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
 @app.route('/prime', methods=['GET'])
 def prime():
-    n = int(request.args.get('n'))
+    n = request.args.get('n')
     if n is None:
         n = 1
-    if n <= 0:
+    if int(n) <= 0:
         return render_template('prime.html', N="Число не является натуральным!", n=n)
     else:
-        if n == 1:
+        if int(n) == 1:
             return render_template('prime.html', N="True", n=n)
         else:
             d = 2
-            while n % d != 0:
+            while int(n) % d != 0:
                 d += 1
-            return render_template('prime.html', N=d == n, n=n)
+            return render_template('prime.html', N=d == int(n), n=n)
 
 
 if __name__ == '__main__':
